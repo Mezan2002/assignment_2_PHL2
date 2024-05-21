daf9426b7ae90d25e70c6aabf9e612e819c5ecd1
mezan2002 <mezanurr009@gmail.com>
2024-05-21 23:23:10 +0600

    interface and model is ready now time to create route , controll the route and create a service logic

---
+++ src/app/modules/products.model.ts
@@ -0,0 +1,53 @@
+import { Schema, model } from "mongoose";
+import { Product } from "./products/products.interface";
+
+// schema for product
+const productSchema = new Schema<Product>({
+  name: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  category: {
+    type: String,
+    required: true,
+  },
+  tags: [
+    {
+      type: String,
+      required: true,
+    },
+  ],
+  variants: [
+    {
+      type: {
+        type: String,
+        required: true,
+      },
+      value: {
+        type: String,
+        required: true,
+      },
+    },
+  ],
+  inventory: {
+    quantity: {
+      type: Number,
+      required: true,
+    },
+    inStock: {
+      type: Boolean,
+      required: true,
+    },
+  },
+});
+
+// model for product
+export const productModel = model<Product>("Product", productSchema);
