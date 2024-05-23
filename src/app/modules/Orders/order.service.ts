ffd1975f9b24b5e9d0b8ad52b8e375c7e84bafc9
mezan2002 <mezanurr009@gmail.com>
2024-05-23 19:19:15 +0600

    issues resolved

--- src/app/modules/Orders/order.service.ts
+++ src/app/modules/Orders/order.service.ts
@@ -3,10 +3,6 @@ import { orderModel } from "../orders.model";
 import { Orders } from "./orders.interface";
 
 const createOrderIntoDB = async (order: Orders) => {
-  const isValidOrderId = mongoose.Types.ObjectId.isValid(order.productId);
-  if (!isValidOrderId) {
-    return null;
-  }
   const result = await orderModel.create(order);
   return result;
 };
